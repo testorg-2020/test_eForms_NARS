90b02331a315fe9490b2906503f21c6c2398c3f7
elinorkeith <keith.elinor@epa.gov>
2021-03-19 15:19:23 -0400

    again

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+echo "Started"
+
 # RUN apt-get update -y
 # RUN apt-get install -y wget gnupg2
 # RUN wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | apt-key add -
