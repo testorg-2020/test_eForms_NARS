3ac338a4ccd74339f5dc53a687100206420fc5fc
elinorkeith <keith.elinor@epa.gov>
2021-03-22 13:18:34 -0400

    Switch to CF7 CLI

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ apt-get install -y wget gnupg2
 wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | apt-key add -
 echo "deb https://packages.cloudfoundry.org/debian stable main" | tee /etc/apt/sources.list.d/cloudfoundry-cli.list
 apt-get update -y
-apt-get install cf-cli -y
+apt-get install cf7-cli -y
 cf install-plugin cflocal -f
