c76b3909eab9a3f964d3aaf6602c9977e1aa57ca
elinorkeith <keith.elinor@epa.gov>
2021-03-22 14:16:31 -0400

    install cflocal outside sudo

--- .github/build.sh
+++ .github/build.sh
@@ -8,4 +8,3 @@ wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key |
 echo "deb https://packages.cloudfoundry.org/debian stable main" | tee /etc/apt/sources.list.d/cloudfoundry-cli.list
 apt-get update -y
 apt-get install cf7-cli -y
-cf install-plugin cflocal -f
