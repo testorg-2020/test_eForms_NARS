e3fb0c44d749a01689e80e0c1ce7015506ac80e5
elinorkeith <keith.elinor@epa.gov>
2021-03-22 13:08:28 -0400

    one more try

--- build.sh
+++ build.sh
@@ -2,14 +2,6 @@
 
 echo "Started"
 
-# RUN apt-get update -y
-# RUN apt-get install -y wget gnupg2
-# RUN wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | apt-key add -
-# RUN echo "deb https://packages.cloudfoundry.org/debian stable main" | tee /etc/apt/sources.list.d/cloudfoundry-cli.list
-# RUN apt-get update -y
-# RUN apt-get install cf-cli -y
-# RUN cf install-plugin cflocal -f
-
 apt-get update -y
 apt-get install -y wget gnupg2
 wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | apt-key add -
