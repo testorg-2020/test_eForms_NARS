69b5293e3b6ad4834b0eb7a114484f005cbdc209
elinorkeith <keith.elinor@epa.gov>
2021-03-24 10:10:08 -0400

    fixed typo, added character restriction to app_name from local.yml

--- .github/get_app_name.py
+++ .github/get_app_name.py
@@ -1,9 +1,15 @@
 import os
+import re
+
+def only_allowed_characters(inputString):
+    return "".join(re.split("[^a-zA-Z0-9_-]*", inputString))
 
 os.chdir(os.environ["GITHUB_WORKSPACE"])
 
 f = open("local.yml", "r")
 for line in f.readlines():
     if line.startswith("- name:"):
-        print("app_name=" + line.split(":")[1].strip())
+        name_from_local_yml = line.split(":")[1].strip()
+        app_name = only_allowed_characters(name_from_local_yml)
+        print("app_name=" + app_name)
 f.close()
