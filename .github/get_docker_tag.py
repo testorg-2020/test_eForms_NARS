83adbce2e1618ff20bf5f6c82f7747743e725c7a
elinorkeith <keith.elinor@epa.gov>
2021-03-24 09:48:24 -0400

    fix docker tag

---
+++ .github/get_docker_tag.py
@@ -0,0 +1,7 @@
+import os
+
+app_name = os.environ["env.APP_NAME"]
+repo = lower(os.environ["GITHUB_REPOSITORY"])
+run_id = os.environ["GITHUB_RUN_ID"]
+
+print("docker_tag=ghcr.io/" + repo + "/" + app_name + ":" + run_id)
