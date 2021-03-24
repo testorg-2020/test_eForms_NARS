b561b3626db72ace0f87209ae15011db2bce9b03
elinorkeith <keith.elinor@epa.gov>
2021-03-24 12:04:20 -0400

    tagging with latest instead

--- .github/get_docker_tag.py
+++
@@ -1,7 +0,0 @@
-import os
-
-app_name = os.environ["APP_NAME"]
-repo = lower(os.environ["GITHUB_REPOSITORY"])
-run_id = os.environ["GITHUB_RUN_ID"]
-
-print("docker_tag=ghcr.io/" + repo + "/" + app_name + ":" + run_id)
