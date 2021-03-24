b12c74978aab231abadb4466fe81bbcce7dd581d
elinorkeith <keith.elinor@epa.gov>
2021-03-24 13:11:50 -0400

    Trying to reduce image size

--- .github/build.sh
+++ .github/build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-echo "Started"
+apt-get update && apt-get install -y \
+    wget \
+    gpupg2 \
+    && rm -rf /var/lib/apt/lists/*
 
-apt-get update -y
-apt-get install -y wget gnupg2
 wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | apt-key add -
 echo "deb https://packages.cloudfoundry.org/debian stable main" | tee /etc/apt/sources.list.d/cloudfoundry-cli.list
-apt-get update -y
-apt-get install cf7-cli -y
+
+apt-get update && apt-get install -y \
+    cf7-cli \
+    && rm -rf /var/lib/apt/lists/*
